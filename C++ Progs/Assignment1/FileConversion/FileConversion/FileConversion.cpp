622d49545520b7119c3ee1b8091c501aab1fb4ec
mmoulton-nscc <w0440932@nscc.ca>
2021-10-22 20:46:54 -0300

    Add project files.

---
+++ C++ Progs/Assignment1/FileConversion/FileConversion/FileConversion.cpp
@@ -0,0 +1,38 @@
+// FileConversion.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void streamOpen();
+void streamClose();
+void openPrint();
+void openWrite();
+void openConvert();
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+void streamOpen()
+{
+}
+
+void streamClose()
+{
+}
+
+void openPrint()
+{
+}
+
+void openWrite()
+{
+}
+
+void openConvert()
+{
+}
