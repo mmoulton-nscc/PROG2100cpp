287339c0932e41ebaaa10930f3062733fb472545
mmoulton-nscc <w0440932@nscc.ca>
2021-10-23 01:41:31 -0300

    final commit

--- C++ Progs/Assignment1/FileConversion/FileConversion/FileConversion.cpp
+++ C++ Progs/Assignment1/FileConversion/FileConversion/FileConversion.cpp
@@ -4,35 +4,181 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 using namespace std;
 
-void streamOpen();
-void streamClose();
-void openPrint();
-void openWrite();
-void openConvert();
+bool validCpp(string str);
+bool validHTML(string str);
+bool testOpen(string readPath, string writePath);
+void readCppWriteHTML(string readPath, string writePath);
+
+struct ExpertExceptionUse : public exception
+{
+public:
+    const string what()
+    {
+        return "My own personal exception happened.";
+    }
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string filepath;
+    string newfilepath;
+    std::cout << "Welcome to the C++ to HTML conversion program.\n";
+    do
+    {
+        do
+        {
+            std::cout << "Please enter the full filename and path of the cpp file you wish to convert to HTML\nexample: C:/doc/thisfile.cpp\n";
+            std::cin >> filepath;
+        } while (!validCpp(filepath)); //loop til we get a valid cpp name/path
+
+        do
+        {
+            std::cout << "Please enter the full filename and path of the new HTML file you wish to create\nexample: C:/doc/thatfile.html\n";
+            std::cin >> newfilepath;
+        } while (!validHTML(newfilepath)); //loop til valid html name/path
+
+    } while (!testOpen(filepath, newfilepath)); //test opening the files without doing anything just in case something messed up, re-request on failure
+
+    readCppWriteHTML(filepath, newfilepath); //if all the dowhiles pass, attempt to convert file
+
+}   
+
+bool validCpp(string str)
+{
+    
+    if (!str.empty())
+    {
+        regex regexp("^([\\w]\\:\\/)([a-zA-Z0-9\\_\\-\\.\\/]+)(\\.cpp)$"); //https://www.regextester.com this took forever and i'm sure it still doesn't get everything
+        if (regex_match(str, regexp))
+        {
+            return true;
+        }
+    }
+    
+    return false;
 }
 
-void streamOpen()
+bool validHTML(string str)
 {
+    regex regexp("^([\\w]\\:\\/)([a-zA-Z0-9\\_\\-\\.\\/]+)(\\.html)$");
+    if (!str.empty())
+    {
+        if (regex_match(str, regexp))
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
-void streamClose()
+bool testOpen(string readPath, string writePath)
 {
+    ifstream cppFile;
+    ofstream HTMLFile;
+    try
+    {
+        cppFile.open(readPath);
+        
+        if(cppFile.fail())
+        {
+            throw invalid_argument("Error loading cpp file, please check path and filename.");
+        }
+        else
+        {
+            cppFile.close();
+        }
+    }
+    catch (const invalid_argument& e)
+    {
+        cout << e.what();
+        return false;
+    }
+
+    try
+    {
+        HTMLFile.open(writePath);
+
+        if (HTMLFile.fail())
+        {
+            throw ExpertExceptionUse();
+        }
+        else
+        {
+            HTMLFile.close();
+            return true;
+        }
+
+    }
+    catch (ExpertExceptionUse& e)
+    {
+        cout << e.what() << endl;
+        return false;
+    }
+    catch (...)
+    {
+        cout << "Default exception?" << endl;
+        return false;
+    }
 }
 
-void openPrint()
+void readCppWriteHTML(string readPath, string writePath)
 {
-}
+    string line;
+    ifstream cppFile;
+    ofstream HTMLFile;
+    try
+    {
+        cppFile.open(readPath);
 
-void openWrite()
-{
-}
-
-void openConvert()
-{
+        if (cppFile.is_open()) //if successfully opened
+        {
+            HTMLFile.open(writePath);
+            if (HTMLFile.is_open())
+            {
+                HTMLFile << "&lt;PRE&gt;" << endl;
+                while (!cppFile.eof()) //loop to end of cpp file
+                {
+                    getline(cppFile, line);
+                    for (char i : line)
+                    {
+                        string lineChar = string(1, i);
+                        if (lineChar == "<")
+                        {
+                            HTMLFile << "&lt;";
+                        }
+                        else if (lineChar == ">")
+                        {
+                            HTMLFile << "&gt;";
+                        }
+                        else
+                        {
+                            HTMLFile << lineChar;
+                        }
+                    }
+                    HTMLFile << "<br>" << endl; //I wasn't sure if the <br> is necessary, but I'm going off this line in the assignment
+                                                //"You can test your output file by opening it with a web browser. The contents should appear identical to the original source code
+                }
+                HTMLFile << "&lt;/PRE&gt;";
+                HTMLFile.close(); //make sure to close file
+            }
+            else
+            {
+                throw runtime_error("file failed to open");
+            }
+            cppFile.close(); //make sure to close file
+        }
+        else
+        {
+            throw runtime_error("file failed to open");
+        }
+    }
+    catch (runtime_error& e)
+    {
+        cout << e.what();
+    }
+    
 }
