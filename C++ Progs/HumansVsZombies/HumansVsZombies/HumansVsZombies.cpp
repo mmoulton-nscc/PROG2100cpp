67423d659d021fcc148e5bf040e03c04f9ff9555
mmoulton-nscc <w0440932@nscc.ca>
2021-12-14 17:24:07 -0400

    predprey outline

--- C++ Progs/HumansVsZombies/HumansVsZombies/HumansVsZombies.cpp
+++ C++ Progs/HumansVsZombies/HumansVsZombies/HumansVsZombies.cpp
@@ -1,20 +1,44 @@
-// HumansVsZombies.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// Created by W0068332 on 11/21/2021.
+//
+//Example main.cpp
+//This supposes that city->step() calls the move method of each organism in the city
+//in a single pass causing each to perform all tasks that it can.
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include "Organism.h"
+#include "City.h"
+#include "GameSpecs.h"
 
-int main()
+using namespace std;
+
+void ClearScreen()
 {
-    std::cout << "Hello World!\n";
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    City* city = new City();
+    chrono::milliseconds interval(PAUSE_MILLISECONDS);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    while (city->hasDiversity()) { //while both humans and zombies exist
+        this_thread::sleep_for(interval);
+        ClearScreen();
+        city->humansMove();
+        city->zombiesMoveEat();
+        city->humansRecruit();
+        city->zombiesRecruit();
+        city->zombiesStarve();
+        city->countOrganisms(Z or H goes here);
+
+        city->reset(); //resets moved flags
+        city->countOrganisms(Z or H goes here);// run once for each type
+        cout << *city; //prints city
+        cout << "GENERATION " << city->getGeneration() << endl;
+        cout << "HUMANS: " << city->countType(HUMAN_CH) << endl;
+        cout << "ZOMBIES: " << city->countType(ZOMBIE_CH) << endl;
+    }//end while
+}//end main
+
